2f2b9b7d0d9bcca750826e5a49b3793fe980f51c
northmindstudies <northmindstudies@gmail.com>
2026-03-19 18:31:39 -0400

    generalized services; newer look

--- assets/js/services-btn.js
+++ assets/js/services-btn.js
@@ -1,33 +1,33 @@
 /* Math Service */
-const btn1 = document.querySelector("#services-toggle-math");
-const list1 = document.querySelector("#services-list-math");
+const btn1 = document.querySelector("#services-toggle-subjects");
+const list1 = document.querySelector("#services-list-subjects");
 
 btn1.addEventListener("click", () => {
     const isOpen1 = list1.classList.toggle("open");
     btn1.setAttribute("aria-expanded", isOpen1);
-    btn1.textContent = isOpen1 ? "Hide" : "Mathematics";
+    btn1.textContent = isOpen1 ? "Hide" : "Subjects";
 });
 
 
 
 /* Physics Service */
-const btn2 = document.querySelector("#services-toggle-phys");
-const list2 = document.querySelector("#services-list-phys");
+const btn2 = document.querySelector("#services-toggle-instr");
+const list2 = document.querySelector("#services-list-instr");
 
 btn2.addEventListener("click", () => {
     const isOpen2 = list2.classList.toggle("open");
     btn2.setAttribute("aria-expanded", isOpen2);
-    btn2.textContent = isOpen2 ? "Hide" : "Physics";
+    btn2.textContent = isOpen2 ? "Hide" : "Instruction";
 });
 
 
 /* Comp sci Service */
-const btn3 = document.querySelector("#services-toggle-cs");
-const list3 = document.querySelector("#services-list-cs");
+const btn3 = document.querySelector("#services-toggle-college-prep");
+const list3 = document.querySelector("#services-list-college-prep");
 
 btn3.addEventListener("click", () => {
     const isOpen3 = list3.classList.toggle("open");
     btn3.setAttribute("aria-expanded", isOpen3);
-    btn3.textContent = isOpen3 ? "Hide" : "Computer Science";
+    btn3.textContent = isOpen3 ? "Hide" : "College Prep";
 });
 
